e5a45f0b908687738caa8239c72c3d4d949b6278
Don Neill <neilljd@gmail.com>
2024-12-05 12:20:08 -0800

    Code cleanup in cryopress.py

--- detlab/cryopress.py
+++ detlab/cryopress.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
-# -----------------------------------------------------------------------------
-# @file      tpg.py
-# @brief     communcations client for Pfeiffer TPG 361 pressure gauge
-# @author    David Hale <dhale@astro.caltech.edu>
-# @date      2015-09-25
-# @modified  2015-09-25
-# @modified  2015-10-07 corrected timestamp %M to %m
-#
-# -----------------------------------------------------------------------------
+"""
+    @file      tpg.py
+    @brief     communcations client for Pfeiffer TPG 361 pressure gauge
+    @author    David Hale <dhale@astro.caltech.edu>
+    @date      2015-09-25
+    @modified  2015-09-25
+    @modified  2015-10-07 corrected timestamp %M to %m
 
+"""
 
 import errno
 import os
 import sys
-import time
-from datetime import datetime,timedelta
-import socket, select
+import json
+from datetime import datetime
+import socket
+import select
 import argparse
-
+# read config file
+with open('logcryo.json') as cfg_fl:
+    config = json.load(cfg_fl)
 # -----------------------------------------------------------------------------
 # where the pressure gauge is located
 # -----------------------------------------------------------------------------
-host = '131.215.200.34'
-port = 8000
-sock = 'sock'
+host = config['presshost']
+port = config['pressport']
 
 # -----------------------------------------------------------------------------
 # control codes
@@ -37,6 +38,7 @@ ENQ  = b'\x05'
 # @brief  return power status string based on numeric value for SEN,0 command
 # -----------------------------------------------------------------------------
 def power_state(argument):
+    """ Get Power State """
     switcher = {
         0: "gauge cannot be turned on/off",
         1: "gauge turned off",
@@ -49,6 +51,7 @@ def power_state(argument):
 # @brief  return error string based on numeric value for PR1 command
 # -----------------------------------------------------------------------------
 def pressure_error(argument):
+    """ Get Pressure Error String """
     switcher = {
         0: "OK",
         1: "underrange",
@@ -65,14 +68,15 @@ def pressure_error(argument):
 # @brief  read socket until <LF> and return reply
 # -----------------------------------------------------------------------------
 def get_reply():
+    """ Get Reply String """
     while True:
         ready = select.select([sock],[],[],3)
         if ready[0]:
-            ret = sock.recv(1024)
+            reply = sock.recv(1024)
         else:
             print("get_reply: select timeout")
             break
-        if (b'\n' in ret):
+        if b'\n' in reply:
             break
     return ret
 
@@ -81,29 +85,33 @@ def get_reply():
 # @brief  append <CR><LF> to command and send over socket
 # -----------------------------------------------------------------------------
 def send_command(cmd):
+    """ Send Command String """
     cmd = cmd + "\r\n"
     sock.sendall(cmd.encode( 'UTF-8' ))
-    return
 
-def read_pressure(type):
+def read_pressure(read_type):
+    """ Read Pressure String """
     send_command('PR1')
-    ret=get_reply()
-    if (ret==ACK):
+    rep=get_reply()
+    if rep==ACK:
         sock.sendall(ENQ)
     else:
         print("read: didn't receive ACK")
 
     sock.sendall(ENQ)
-    ret=get_reply()
-    ans=ret.decode( 'UTF-8' )
+    rep=get_reply()
+    ans=rep.decode( 'UTF-8' )
     ans=ans.split(',')
 
-    if (type=="log"):
+    if read_type== "log":
         return float(ans[1])*1000.
-    if (type=="read"):
+    if read_type== "read":
         print(float(ans[1])*1000.," mTorr")
-        if (int(ans[0]) != 0):
+        if int(ans[0]) != 0:
             print(pressure_error(int(ans[0])))
+        return None
+
+    return None
 
 # -----------------------------------------------------------------------------
 # @fn     main
@@ -112,20 +120,25 @@ def read_pressure(type):
 if __name__ == "__main__":
 
     parser=argparse.ArgumentParser(description='Pfeiffer TPG 361 controller')
-    parser.add_argument('--power', metavar='[on|off|?]', nargs=1, type=str, help='turn gauge on|off or read state with ?')
-    parser.add_argument('--read', action='store_true', help='read pressure measurement')
-    parser.add_argument('--log', action='store_true', help='log pressure measurement')
-    parser.add_argument('--com', metavar='mnemonic', nargs=1, type=str, help='send mnemonic command string to controller (no spaces!)')
+    parser.add_argument('--power', metavar='[on|off|?]', nargs=1,
+                        type=str, help='turn gauge on|off or read state with ?')
+    parser.add_argument('--read', action='store_true',
+                        help='read pressure measurement')
+    parser.add_argument('--log', action='store_true',
+                        help='log pressure measurement')
+    parser.add_argument('--com', metavar='mnemonic', nargs=1, type=str,
+                        help='send mnemonic command string to controller (no spaces!)')
     args=parser.parse_args()
 
     if len(sys.argv)==1:
         sys.exit(1)
 
+    sock = None
     try:
         # open socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect( (host, port) )
-        sock.setblocking(0)
+        sock.setblocking(False)
 
         # send command to read pressure (the "read" parameter causes printing to stdout)
         if args.read:
@@ -133,14 +146,14 @@ if __name__ == "__main__":
 
         # send command to read pressure
         if args.log:
-            fn        = "/data/log/pressure.log"
+            fn = "pressure.log"
             timestamp = datetime.now().strftime("%Y-%m-%dT%X")
             # read the pressure now (the "log" parameter is non-verbose)
             pressure  = read_pressure("log")
             # if the log file has grown over 1MB in size then rename it and start a new log file
             # (assuming it exists, of course)
-            if (os.path.exists(fn)):
-                if (os.path.getsize(fn) > 1024000):
+            if os.path.exists(fn):
+                if os.path.getsize(fn) > 1024000:
                     os.rename(fn, fn+"."+timestamp)
             # open file for append
             logfile = open(fn, 'a+')
@@ -149,23 +162,23 @@ if __name__ == "__main__":
 
         # turn power on|off
         if args.power:
-            if (args.power[0] == 'on'):
+            if args.power[0] == 'on':
                 send_command('SEN,2,0,0,0,0,0')
-            elif (args.power[0] == 'off'):
+            elif args.power[0] == 'off':
                 send_command('SEN,1,0,0,0,0,0')
             # or query the power status
-            elif (args.power[0] == '?'):
+            elif args.power[0] == '?':
                 send_command('SEN,0,0,0,0,0,0')
             else:
                 print("power: must specify on|off|?")
                 sys.exit(1)
             ret=get_reply()
-            if (ret==ACK):
+            if ret==ACK:
                 sock.sendall(ENQ)
                 ret=get_reply()
-                ans = ret.decode( 'UTF-8' )
-                ans = ans.split(',')
-                for i, stat in enumerate(ans):
+                answer = ret.decode( 'UTF-8' )
+                answer = answer.split(',')
+                for i, stat in enumerate(answer):
                     print("Sen %d: %s" % (i+1, power_state(int( stat ))))
             else:
                 print("power: didn't receive ACK")
@@ -174,7 +187,7 @@ if __name__ == "__main__":
         if args.com:
             send_command(args.com[0])
             ret=get_reply()
-            if (ret==ACK):
+            if ret==ACK:
                 sock.sendall(ENQ)
             else:
                 print("com: didn't receive ACK")
@@ -185,4 +198,5 @@ if __name__ == "__main__":
         if se.errno == errno.EHOSTUNREACH:
             print("error")
     finally:
-        sock.close()
+        if sock:
+            sock.close()
